a6568f04c5d381998dc99f8d1e4f3d3ff5a1659a
Mahesh-12882 <kumarmaheshmk0786@gmail.com>
2021-06-05 19:25:12 +0530

    fixed route and config on server

--- src/config/config.js
+++ src/config/config.js
@@ -9,9 +9,9 @@ module.exports = {
 	db_name: 'imgn',
 
 
-	db_path_local: 'synopsis-db.cxrrvbjsckdc.us-east-2.rds.amazonaws.com',
-	db_root_local: 'admin',
-	db_pass_local: 'Creative25',
+	db_path_local: 'localhost',
+	db_root_local: '', // ok. and then lets do it later. just a sec
+	db_pass_local: '',
 
 	breakdown_server: true,
 	
@@ -24,23 +24,37 @@ module.exports = {
 	max_shooting_duration: 8,
 	env: 'development',
 
+  aws_config: {
+    accessKeyId: 'AKIAJPH5OEC3JDFW5CSA',
+    secretAccessKey: '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',
+    region: 'us-east-2'
+  },
+  a1ws_apiVersion: '2010-08-01',
+  
+  redis_host: 'redis-17871.c8.us-east-1-3.ec2.cloud.redislabs.com',
+  redis_port: 17871,
+  redis_pass: 'stavring1!',
+  
+  s3: 1,
+  aws_access_key_id: 'AKIAJPH5OEC3JDFW5CSA',
+  aws_secret_access_key: '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',
+  aws_access_key_id_deepnen: 'AKIAJVIHZQ6R7VWVS3OA',
+  aws_secret_access_key_deepnen: '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',
+  s3_bucket_name: 'imgn',
+  s3_url_prefix: 'https://imgn.s3.us-east-2.amazonaws.com/',
+  
+  mailer_provider: 'gmail',
+  mailer_host: 'smtp.gmail.com',
+  mailer_params: {
+    
+    user: 'info@imgn.co',
+    pass: 'stav1122'
+  }
+
 	
 }
 
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize('imgn','mahesh','mahesh',{
-//   // The `host` parameter is required for other databases
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   pool : {max:5,min:0,ide:1000}
-// });
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
+
+
 
